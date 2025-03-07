8ba9a9026c285ce0eddcced0025db059a6e5ace9
Zsolt Zsupos <zsoltzsupos@Zsolts-Laptop.local>
2025-03-07 13:25:54 +0100

    for and if added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, computed, Output, EventEmitter } from '@angular/core';
 
+interface User  {
+  id: string,
+  avatar: string,
+  name: string
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,21 +14,22 @@ import { Component, Input, computed, Output, EventEmitter } from '@angular/core'
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
+  //@Input({required: true}) avatar!: string;
+  //@Input({required: true}) name!: string;
   //avatar = input.required<string>();
   //name = input.required<string>();
-  @Input({required:true}) id!: string;
+  //@Input({required:true}) id!: string;
   //id = input.required<string>();
   @Output() select = new EventEmitter<string>();
   //select = output<string>();
 
 
   imagePath = computed(() => {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   })
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
